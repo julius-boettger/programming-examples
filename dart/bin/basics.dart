341b19af71840f2e33b2b8db17c59d9e07b70e80
julius-boettger <boettger.julius@gmail.com>
2023-06-18 21:37:14 +0200

    dart: added examples for variables

--- dart/bin/basics.dart
+++ dart/bin/basics.dart
@@ -1,5 +1,12 @@
-/** examples for basic syntax and concepts */
+// the following is a doc-comment:
+/// examples for basic syntax and concepts
 void basicExamples() {
     // print hello world to the console
     print("Hello World!");
+
+    //// variables
+    // the type of a variable will be inferred when using "var":
+    var firstVariable = 0;
+    // the type can also be annotated manually:
+    int secondVariable = firstVariable;
 }
