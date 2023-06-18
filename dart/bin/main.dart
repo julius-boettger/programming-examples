2974bc6d9dd1d804a35a6e3dadc459efa4f9c22d
julius-boettger <boettger.julius@gmail.com>
2023-06-18 20:49:27 +0200

    dart: added project

---
+++ dart/bin/main.dart
@@ -0,0 +1,17 @@
+/*
+
+SETUP
+- get dart: https://dart.dev/get-dart
+    - on non-debian-based linux:
+        - download zip archive
+        - in your shell configuration: export PATH="$PATH:/path/to/dart-sdk/bin"
+- create new project: dart create project-name
+- run project: dart run
+
+*/
+
+// entry point of the program. the file containing this needs to be specified in pubspec.yaml
+void main() {
+    // print hello world to the console
+    print("Hello World!");
+}
