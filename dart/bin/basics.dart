4f629b20747c97d09486daeae8a1cadc2d8109b0
julius-boettger <boettger.julius@gmail.com>
2023-06-20 20:24:22 +0200

    dart: added more basic examples

--- dart/bin/basics.dart
+++ dart/bin/basics.dart
@@ -9,4 +9,28 @@ void basicExamples() {
     var firstVariable = 0;
     // the type can also be annotated manually:
     int secondVariable = firstVariable;
+    // a questionmark allows a variable to be null
+    int? nullable = null;
+
+    //// statements
+    if (true) {
+        // do something
+    } else if (false) {
+        // do something else
+    }
+
+    //// loops
+    for (int i = 1; i < 10; i++) {
+        // do something
+    }
+
+    // final is a good practice and makes the iterating variable immutable
+    for (final number in [1, 2, 3]) {
+        // the following is not allowed!
+        // number = 1;
+    }
+
+    while (false) {
+        // do nothing!
+    }
 }
