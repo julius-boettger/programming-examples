1c353e2ccf2ab6bfe32c214835efe0cd81fade7e
julius-boettger <boettger.julius@gmail.com>
2023-07-10 15:30:00 +0200

    rust: added examples for match

--- rust/src/control.rs
+++ rust/src/control.rs
@@ -1,8 +1,6 @@
 /// examples of control structures and general flow of control
 pub fn run_examples() {
     //// if, else if, else
-    // blocks are expressions + after "if" comes a block => if-expressions!
-
     if true {
         "the boolean expression is true!";
     } else if false {
@@ -10,14 +8,36 @@ pub fn run_examples() {
     } else {
         "and this is also unecessary";
     }
-
+    // blocks are expressions and after "if" comes a block => if-expressions!
     let returned_by_if = if true {
         "true"
     } else {
         "false"
     };
 
-    //// loop 
+    //// match
+    // like "switch" in other languages, but all possible options must be covered
+    // only evaluates the first matching "arm" ("case" in other languages)
+    let number = -2;
+    let result: &str = match number { // is also an expression!
+        0 => "yeah pretty cool number",
+        // ranges
+        1..=1 => "haha, haha, one!",
+        // "bindings": assign name to value to use it when evaluating the arm
+        // (-1 could also be a range)
+        n @ -1 => {
+            // can also use blocks here
+            let x = n + 1;
+            "yeeeah pretty cool number"
+        },
+        // "guards": use boolean to match value
+        // (value is also assigned a name, like with bindings)
+        n if n >= 0 => "thats... positive",
+        // match all possible values, should be last arm
+        _ => "idk what that is"
+    };
+
+    //// loops
     // loop: executes indefinitely until a break is reached
     let mut i = 0;
     loop {
@@ -40,6 +60,6 @@ pub fn run_examples() {
     for j in 1..=3 {
         // j will be 1, 2, 3 (see ranges)
     }
-    // this would do something 3 times wi
+    // this would do something 3 times with no iterating variable
     for _ in 0..3 {}
 }
