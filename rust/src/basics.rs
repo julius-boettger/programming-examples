55e4e849afbe4377fbdaf46d971d5b2090ce8d15
julius-boettger <boettger.julius@gmail.com>
2023-09-04 23:15:10 +0200

    rust: improve some comments

--- rust/src/basics.rs
+++ rust/src/basics.rs
@@ -192,12 +192,12 @@ pub fn run_examples() {
     assert_eq!(block_with_value, 4);
 }
 
-// module for unit tests, run them with "cargo test"
+// module for unit testing the parent module
+// run with "cargo test"
 #[cfg(test)]
 mod tests {
-    // use stuff from outer scope
+    // use stuff from parent scope, often useful
     use super::*;
-
     // a test case will fail if it panics
     #[test] fn will_pass() { assert_eq!(2 + 2, 4); }
     #[test] fn will_fail() { panic!(); }
