031258c821ea4a166454bec8f1cd16ab29ab6b84
julius-boettger <boettger.julius@gmail.com>
2023-09-06 09:27:18 +0200

    rust: add more trait examples

--- rust/src/traits.rs
+++ rust/src/traits.rs
@@ -46,22 +46,61 @@ pub fn run_examples() {
     // a trait is a collection of methods that can be implemented by a type
     trait Pet {
         // a function doesn't need to have a body
-        fn name(&self) -> &String;
+        fn name(&self) -> String;
         // but it can have one to provide a default implementation
         fn noise() -> String {
             String::from("*weird noise*")
         }
     }
+
     // Gecko needs to implement all of Pet's methods in this implementation block
     impl Pet for Gecko {
-        fn name(&self) -> &String {
-            &self.name
+        fn name(&self) -> String {
+            self.name.clone()
         }
 
         fn noise() -> String {
             String::from("...")
         }
     }
+
+    // another trait
+    trait Dangerous {
+        fn get_danger_level() -> u8;
+    }
+
+    // a subtrait, which is a combination of at least one supertrait
+    // implementing a subtrait also requires implementing all supertraits!
+    trait DangerousPet: Dangerous + Pet {
+        fn is_good_pet() -> bool;
+    }
+
+    // a komodo dragon is a dangerous pet
+    struct KomodoDragon;
+    // implement subtrait
+    impl DangerousPet for KomodoDragon {
+        fn is_good_pet() -> bool { false }
+    }
+    // implement its supertraits
+    impl Dangerous for KomodoDragon {
+        fn get_danger_level() -> u8 { 255 }
+    }
+    impl Pet for KomodoDragon {
+        fn name(&self) -> String {
+            String::from("(unnamed komodo dragon)")
+        }
+    }
+
+    //// working with overlapping traits
+    // it's possible for a type to implement functions with the same name from different traits
+    struct Confusing;
+    trait Trait1 { fn get_string() -> String { "from Trait1".to_owned() } }
+    trait Trait2 { fn get_string() -> String { "from Trait2".to_owned() } }
+    impl Trait1 for Confusing {}
+    impl Trait2 for Confusing {}
+    // decice which implementation to use through fully qualified syntax
+    assert_eq!(<Confusing as Trait1>::get_string(), "from Trait1");
+    assert_eq!(<Confusing as Trait2>::get_string(), "from Trait2");
 
     //// the derive attribute
     // rust provides basic implementation for some traits
@@ -86,8 +125,8 @@ pub fn run_examples() {
         // changes move-semantics to copy-semantics, where a bit-wise copy is executed
         // also requires Clone
         Copy,
-        // a more generic version of Copy where you can run
-        // some code to get a duplicate of an instance
+        // a more generic version of Copy that implements
+        // clone() to get a duplicate of an instance
         Clone,
         // makes type able to be hashed by a Hasher
         Hash,
@@ -98,4 +137,22 @@ pub fn run_examples() {
     )]
     struct DoItAll;
 
+    // Drop trait: only method drop() is called when an object goes out of scope
+    struct Droppable;
+    impl Drop for Droppable {
+        fn drop(&mut self) {
+            println!("a Droppable was dropped!");
+        }
+    }
+
+    // Iterator trait: only method into_iter()
+    struct RangeWrapper(std::ops::Range<i32>);
+    impl Iterator for RangeWrapper {
+        type Item = i32; // type of iterating items
+        fn next(&mut self) -> Option<Self::Item> {
+            self.0.next()
+        }
+    }
+    // x = 1, 2, 3, 4
+    for x in RangeWrapper(0..5).into_iter() {}
 }
