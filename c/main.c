ec231834ffe486a8d7de257cadcbc513cfc0960b
julius-boettger <julius.btg@proton.me>
2024-02-06 21:21:48 +0100

    c: init project

---
+++ c/main.c
@@ -0,0 +1,13 @@
+// import standard i/o functionality like printf().
+// ".h" is for a speacial "header" file, which does not
+// contain actual function implementations,
+// but is more like a contract or interface
+#include <stdio.h>
+
+// entry point of the program
+// returns a "return code" as an integer, usually 0
+int main () {
+    printf("hello world!");
+    // indicate successful program execution and planned exit
+    return 0;
+}
