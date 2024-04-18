8772665ab9bd5cfaa1b5a9dda50e20d362c9e1d0
julius-boettger <julius.btg@proton.me>
2024-04-18 22:30:56 +0200

    cpp: add inline functions

--- cpp/src/include/examples.h
+++ cpp/src/include/examples.h
@@ -1,6 +1,13 @@
 #ifndef EXAMPLES_H
 #define EXAMPLES_H
 
+// function definitions can be supplied with "inline"
+// in header files without violating the one-definiton-rule.
+// other, more traditional use of this keyword is deprecated.
+inline int defined_in_header () {
+    return 32;
+}
+
 // namespaces need to be considered here!
 namespace basics { void run_examples(); }
 
