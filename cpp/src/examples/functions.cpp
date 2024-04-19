63ac3cb3d54419f0de1c962b359d6d8d0ded5591
julius-boettger <julius.btg@proton.me>
2024-04-19 08:35:09 +0200

    cpp: add consteval

--- cpp/src/examples/functions.cpp
+++ cpp/src/examples/functions.cpp
@@ -1,6 +1,8 @@
 // functions that can be evaluated at compile
-// time should also be marked with constexpr
-constexpr int get_special_value() {
+// time should also be marked with "consteval".
+// "constexpr" is also possible, but does
+// not enforce compile time evaluation.
+consteval int get_special_value() {
     return 123 * 23 - 592 + 1111;
 }
 
