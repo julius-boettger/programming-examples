a828ef46243959c16fb185a821ad8815e3ff0c07
julius-boettger <julius.btg@proton.me>
2024-04-19 08:50:25 +0200

    cpp: improve consteval comments

--- cpp/src/examples/functions.cpp
+++ cpp/src/examples/functions.cpp
@@ -1,13 +1,20 @@
-// functions that can be evaluated at compile
-// time should also be marked with "consteval".
-// "constexpr" is also possible, but does
-// not enforce compile time evaluation.
-consteval int get_special_value() {
+// "constexpr" functions will be evaluated
+// at compile time wherever possible
+constexpr int get_special_value() {
+    return 5555 + 1 - 4000 * 2342;
+}
+// "consteval" functions must be evaluated at
+// compile time wherever they are used
+consteval int get_other_special_value() {
     return 123 * 23 - 592 + 1111;
 }
+// both "constexpr" and "consteval" functions are
+// implicitly inline and should be defined in header
+// files if they are used by multiple source files!
 
 namespace functions {
     void run_examples() {
         constexpr int special_value { get_special_value() };
+        constexpr int other_special_value { get_other_special_value() };
     }
 }
