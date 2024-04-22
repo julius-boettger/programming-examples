a12a9929306466c86132715053f3452f458986da
julius-boettger <julius.btg@proton.me>
2024-04-22 09:43:20 +0200

    cpp: add inline namespaces

--- cpp/src/examples/namespaces.cpp
+++ cpp/src/examples/namespaces.cpp
@@ -4,7 +4,7 @@ void do_nothing() {}
 // convenient syntax for nested namespaces
 namespace one::two {}
 
-//// anonymous/unnamed namespace:
+//// anonymous/unnamed namespace
 // treated as if part of parent namespace (here: global)
 // makes identifiers only visible to this file (like static)
 // but is considered better practice than using static
@@ -12,15 +12,30 @@ namespace {
     const int not_static {};
 }
 
+//// inline namespace
+// useful for versioning implementations of functions for
+// backward compatability, marking the current default as inline.
+// see below for more on how to call these functions!
+       namespace v1 { void important_function() {} }
+inline namespace v2 { void important_function() {} }
+
 namespace namespaces {
     void run_examples() {
-        // call function explicitly from global namespace:
+        //// more on inline namespaces
+        // explicitly call a specific implementation
+        v1::important_function();
+        v2::important_function();
+        // call the current default (inline) implementation
+        important_function();
+
+        //// call function explicitly from global namespace
         // can be useful when multiple namespaces
         // have functions with the same name
         ::do_nothing();
         // in this example, you can also just...
         do_nothing();
-        // namespace aliases exist
+
+        //// namespace aliases (exist)
         namespace shorthand = one::two;
     }
 }
