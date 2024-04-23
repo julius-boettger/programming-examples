7806df0eca3e16361a1ea9e6521d0a85c483e6ec
julius-boettger <julius.btg@proton.me>
2024-04-23 11:01:06 +0200

    cpp: add function default arguments

--- cpp/src/examples/functions.cpp
+++ cpp/src/examples/functions.cpp
@@ -1,14 +1,15 @@
-// "constexpr" functions will be evaluated
+//// constexpr and consteval functions
+// constexpr" functions will be evaluated
 // at compile time wherever possible
 constexpr int get_special_value() {
     return 5555 + 1 - 4000 * 2342;
 }
-// "consteval" functions must be evaluated at
+// consteval functions must be evaluated at
 // compile time wherever they are used
 consteval int get_other_special_value() {
     return 123 * 23 - 592 + 1111;
 }
-// both "constexpr" and "consteval" functions are
+// both constexpr and consteval functions are
 // implicitly inline and should be defined in header
 // files if they are used by multiple source files!
 
@@ -19,6 +20,13 @@ double add(double x, double y) { return x + y; }
 // most useful when working with overloaded functions
 // (but also not that useful?)
 int add(long, long) = delete;
+
+//// default arguments
+void default_arguments(int x = 5) {};
+// can either be in the function
+// prototype (preferred) or definition! not both!
+void default_arguments2(int x = 5); // prototype
+void default_arguments2(int x) {}   // definition
 
 namespace functions {
     void run_examples() {
