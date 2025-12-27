6cfbd283c34cd4f085dab435245e235f4840e113
julius-boettger <julius.btg@proton.me>
2025-12-27 21:45:37 +0100

    hs: init

---
+++ haskell/app/Main.hs
@@ -0,0 +1,6 @@
+-- run with `cabal run`
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ haskell/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    systems.url = "github:nix-systems/default"; # can run on all systems
+  };
+
+  outputs = { self, nixpkgs, systems, ... }:
+  let
+    eachSystem = fn: nixpkgs.lib.genAttrs (import systems) (system: fn system (import nixpkgs {
+      inherit system;
+    }));
+  in
+  {
+    devShells = eachSystem (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+            (ghc.withPackages (pkgs: [ pkgs.cabal-install ]))
+        ];
+      };
+    });
+  };
+}
