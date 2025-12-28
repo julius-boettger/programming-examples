325d9a0eb78227ee0e645e01d72edb518af0c4b5
julius-boettger <julius.btg@proton.me>
2025-12-28 17:16:49 +0100

    hs: downgrade haskell version

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    # for ghc 9.8.4 as its the last version where the hls hlint plugin still works
+    nixpkgs.url = "github:nixos/nixpkgs?rev=f4b140d5b253f5e2a1ff4e5506edbf8267724bde";
     systems.url = "github:nix-systems/default"; # can run on all systems
   };
 
