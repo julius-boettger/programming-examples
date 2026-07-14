29d252fe9bda396602d7e3bc084533e1c44a2b90
julius-boettger <julius.btg@proton.me>
2026-07-14 22:37:28 +0200

    jl: more structs

--- julia/src/Structs.jl
+++ julia/src/Structs.jl
@@ -4,12 +4,20 @@ struct Struct
     # types are optional
     field1::Int
     field2::Bool
+    # default constructor definition
+    #Struct(field1, field2) = new(field1, field2)
 end
 
+# use default constructor
 object = Struct(0, true)
 @assert object.field1 == 0
 # not allowed: struct is immutable
 #object.field1 = 1
+
+# add constructor without arguments
+Struct() = Struct(0, false)
+defaultObj = Struct()
+@assert defaultObj.field2 == false
 
 # a mutable struct (objects are mutable)
 mutable struct mutableStruct
@@ -20,3 +28,11 @@ end
 mutableObj = mutableStruct(0, 0)
 mutableObj.field = 1
 @assert mutableObj.field == 1
+
+# with a parametric type
+struct Parametric{T}
+    x::T
+    y::T
+end
+parametric = Parametric{Int}(1, 1)
+@assert typeof(parametric.x) == Int
